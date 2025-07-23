a37b0b5111b36c4647f3066315b7fd0adc4c3422
pseudoerr0x213 <20chryskylodon09@gmail.com>
2025-07-22 20:42:21 -0700

    chat api

--- pkg/kick/chat.go
+++ pkg/kick/chat.go
@@ -2,7 +2,13 @@ package kick
 
 import (
 	"context"
-	"fmt"
+	"errors"
+)
+
+var (
+	ErrMissingContent       = errors.New("content is required")
+	ErrMissingBroadcasterID = errors.New("broadcaster_user_id is required")
+	ErrInvalidMessageType   = errors.New("type must be \"user\" or \"bot\"")
 )
 
 // type field can only have two values: "user" or "bot". See: https://docs.kick.com/apis/chat
@@ -14,9 +20,9 @@ const (
 )
 
 type SendChatRequest struct {
-	BroadcasterUserID int64           `json:"broadcaster_user_id"`
+	BroadcasterUserID int64           `json:"broadcaster_user_id,omitempty"` //optional
 	Content           string          `json:"content"`
-	ReplyToMessageID  string          `json:"reply_to_message_id"`
+	ReplyToMessageID  string          `json:"reply_to_message_id,omitempty"` //optional
 	Type              ChatMessageType `json:"type"`
 }
 type SendChatResponse struct {
@@ -28,49 +34,51 @@ func (c *Client) PostChatMessage(
 	ctx context.Context,
 	broadcasterUserID int64,
 	content string,
-	reply_to_msg_id string, msgType string) (*SendChatResponse, error) {
-	var req SendChatRequest
+	msgType ChatMessageType,
+	opts ...SendChatRequestOption,
+) (*SendChatResponse, error) {
 
-	switch msgType {
-	case "user":
-		msgType = ChatMessageType(ChatMessageUser)
-
-		if msgType == ChatMessageType(ChatMessageUser) && broadcasterUserID == 0 {
-			return nil, fmt.Errorf("broadcaster userID is required")
-		}
-
-	case "bot":
-		msgType = ChatMessageBot(ChatMessageBot)
-
-	default:
-		return nil, fmt.Errorf("the msgType should be user or bot")
+	req, err := NewSendChatRequest(content, msgType, opts...)
+	if err != nil {
+		return nil, err
 	}
 
-	return req, nil
+	req.BroadcasterUserID = broadcasterUserID
+
+	if req.Content == "" {
+		return nil, ErrMissingContent
+	}
+
+	path := "/public/v1/chat/message"
+	var resp SendChatResponse
+	if err := c.doRequest(ctx, "POST", path, req, &resp); err != nil {
+		return nil, err
+	}
+	return &resp, nil
 }
 
 type SendChatRequestOption func(*SendChatRequest)
 
-func NewSendChatRequest(broadcasterUserID int64, content string, reply_to_msg_id string, msgType string) (*SendChatRequest, error) {
-	switch msgType {
-	case "user":
-		msgType = ChatMessageType(ChatMessageUser)
-	case "bot":
-		msgType = ChatMessageBot(ChatMessageBot)
-
-	default:
-		return nil, fmt.Errorf("the msgType can be user or bot")
+func NewSendChatRequest(
+	content string,
+	msgType ChatMessageType,
+	opts ...SendChatRequestOption,
+) (*SendChatRequest, error) {
+	if content == "" {
+		return nil, ErrMissingContent
 	}
-
+	if msgType != ChatMessageUser && msgType != ChatMessageBot {
+		return nil, ErrInvalidMessageType
+	}
 	return &SendChatRequest{
-		BroadcasterUserID: broadcasterUserID,
-		Content:           content,
-		ReplyToMessageID:  reply_to_msg_id,
-	}
-
+		Content: content,
+		Type:    msgType,
+	}, nil
 }
 
-// constructor for sending chat messages from user
-func UserSendChatRequest(broadcasterUserID int64, content string, replyToMsg string) *SendChatRequestOption {
-
+// use this if you want to send a reply to
+func WithReplyToMessageID(id string) SendChatRequestOption {
+	return func(r *SendChatRequest) {
+		r.ReplyToMessageID = id
+	}
 }
